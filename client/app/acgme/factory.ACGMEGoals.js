60055aa715dc17ac0ff5d6230e0e6650efabff58
L. Leon Chen <lchen3@gmail.com>
2014-11-04 20:03:23 -0500

    ACGME view

--- client/app/acgme/factory.ACGMEGoals.js
+++ client/app/acgme/factory.ACGMEGoals.js
@@ -2,14 +2,31 @@
 
 var app = angular.module('troveApp');
 
-app.factory('ACGMEGoals', function($http) {
+app.factory('ACGMEGoals', function($http, $q) {
 
     return {
-        getACGMEGoals: function() {
-            // Loads the json file for ACGME goals
-            return $http.get('/assets/data/ACGME_Goals.json').then(function(results) {
+        getACGMEGoals: function(userId) {
 
-                return results;
+            // Loads the json file for ACGME goals
+            return $http.get('/assets/data/ACGME_Goals.json').then(function(acgme_goals) {
+
+                var acgme_goals_array = acgme_goals.data;
+
+                var study_indices = [];
+                for (var i = 0; i < acgme_goals_array.length; i++) {
+                    study_indices.push(acgme_goals_array[i].study_index);
+                }
+
+                return $q.all(study_indices.map(function (study_index) {
+                    return $http.get('/api/users/' + userId + '/ACGME/' + study_index);
+                })).then(function (results) {
+                    for (var i=0; i<results.length; i++) {
+                        acgme_goals_array[i].user_number = results[i].data;
+                    }
+
+                    return acgme_goals_array;
+                });
+
             });
         }
    }
